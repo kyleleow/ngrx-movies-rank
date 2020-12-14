28b9cb08fa24bf6155bf5ad12e8c3a10129c0e8a
KengYe Leow <leow.kengye@growthops.asia>
2020-12-14 23:09:55 +0800

    test: fix provider for app list test

--- src/app/app-list/app-list.component.spec.ts
+++ src/app/app-list/app-list.component.spec.ts
@@ -1,19 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { MovieService } from '../movie.service';
 import { AppListComponent } from './app-list.component';
 
 describe('AppListComponent', () => {
   let component: AppListComponent;
   let fixture: ComponentFixture<AppListComponent>;
+  const initialState = { movies: [] };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AppListComponent ]
+      imports: [HttpClientTestingModule],
+      declarations: [ AppListComponent ],
+      providers: [
+        provideMockStore({initialState}),
+        MovieService
+      ]
     })
     .compileComponents();
-  });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(AppListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
